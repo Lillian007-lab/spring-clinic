edad4c87908f778966c0f070c510e868ab371082
Tianyi Zhou <zcarol.530@gmail.com>
2021-05-30 00:56:29 -0400

    Add BaseEntity

--- clinic-data/src/main/java/com/example/springclinic/model/Pet.java
+++ clinic-data/src/main/java/com/example/springclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.example.springclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 
     private PetType petType;
     private Owner owner;
