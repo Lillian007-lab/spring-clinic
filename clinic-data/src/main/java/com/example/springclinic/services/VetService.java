062310ea9a9cbcfecacd3eeade154e4486a41608
Tianyi Zhou <zcarol.530@gmail.com>
2021-05-30 01:02:37 -0400

    add CrudService interface and refactor

--- clinic-data/src/main/java/com/example/springclinic/services/VetService.java
+++ clinic-data/src/main/java/com/example/springclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.example.springclinic.services;
 
 import com.example.springclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
 }
