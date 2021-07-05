a46ef203ea42150e6b47cac2cf3beef3fcd7bab5
Tianyi Zhou <zcarol.530@gmail.com>
2021-07-04 20:39:50 -0400

    added processFindFormEmptyReturnMany() in OwnerControllerTest

--- clinic-web/src/test/java/com/example/springclinic/controllers/OwnerControllerTest.java
+++ clinic-web/src/test/java/com/example/springclinic/controllers/OwnerControllerTest.java
@@ -75,7 +75,7 @@ class OwnerControllerTest {
     }
 
     @Test
-    void processFindFOrmReturnMany() throws Exception {
+    void processFindFormReturnMany() throws Exception {
         when(ownerService.findAllByLastNameLike(anyString()))
                 .thenReturn(Arrays.asList(Owner.builder().id(1L).build(),
                         Owner.builder().id(2L).build()));
@@ -84,6 +84,19 @@ class OwnerControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("owners/ownersList"))
                 .andExpect(model().attribute("selections", hasSize(2)));
+    }
+
+    @Test
+    void processFindFormEmptyReturnMany() throws Exception {
+        when(ownerService.findAllByLastNameLike(anyString()))
+                .thenReturn(Arrays.asList(Owner.builder().id(1l).build(),
+                        Owner.builder().id(2l).build()));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/owners")
+                .param("lastName",""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownersList"))
+                .andExpect(model().attribute("selections", hasSize(2)));;
     }
 
     @Test
